f8a5c25701ebe7ab08faf34c10fe1799679842b4
keibou7 <keibou.address@gmail.com>
2020-06-05 15:46:20 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,5 +14,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'backet-keibou'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/backet-keibou'
-  config.fog_public = false
 end
